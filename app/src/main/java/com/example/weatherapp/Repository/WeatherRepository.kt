096e72f1837ee58f84ad8c146ab31f2d82cf30fa
vishal5540 <vishalbhushan1232@gmail.com>
2024-07-08 19:39:09 +0530

    Model and Repository part is completed.

---
+++ app/src/main/java/com/example/weatherapp/Repository/WeatherRepository.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.Repository
+
+import com.example.weatherapp.Server.ApiServices
+
+class WeatherRepository(val api:ApiServices) {
+
+    fun getCurrentWeather(lat: Double,lng:Double,unit:String)=
+        api.getCurrentWeather(lat,lng,unit,"0773ade61d7e97ff9b2d9a906d7670bf")
+
+    fun getForecastWeather(lat: Double,lng:Double,unit:String)=
+        api.getForecastWeather(lat,lng,unit,"0773ade61d7e97ff9b2d9a906d7670bf")
+}
